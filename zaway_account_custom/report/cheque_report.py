77c184fc58fcc406cd90a01d0a109b5145a0a482
Malaz Mohammed <malaz.mohammed@iatl-intellisoft.com>
2022-11-30 14:46:53 +0200

    report

--- zaway_account_custom/report/cheque_report.py
+++ zaway_account_custom/report/cheque_report.py
@@ -11,15 +11,6 @@ class ChequeReport(models.AbstractModel):
     def _get_header_info(self, data):
         date_from = data['date_from']
         date_to = data['date_to']
-        # under_collection = data['under_collection']
-        # in_bank = data['in_bank']
-        # rejected = data['rejected']
-        # return_to = data['return_to']
-        # out_standing = data['out_standing']
-        # withdrawal = data['withdrawal']
-        # done = data['done']
-        # cancel = data['cancel']
-        customer_name = data['customer_name']
         customer_id = data['customer_id']
         state = data['state']
 
@@ -27,24 +18,12 @@ class ChequeReport(models.AbstractModel):
             'date_from': date_from,
             'date_to': date_to,
             'customer_id': customer_id,
-            'customer_name': customer_name,
-            # 'under_collection': under_collection,
-            # 'rejected': rejected,
-            # 'in_bank' : in_bank,
-            # 'return_to': return_to,
-            # 'out_standing': out_standing,
-            # 'withdrawal': withdrawal,
-            # 'done': done,
-            # 'cancel': cancel,
             'state': state,
             # 'company_id': company_id,
 
         }
 
     def _get_cheque(self, data):
-
-        # customer_list =[]
-        # check_list = []
 
         if data['date_from'] > data['date_to']:
             raise UserError(_("You must enter a start date less than the end date."))
@@ -54,21 +33,98 @@ class ChequeReport(models.AbstractModel):
             ('Date', '>=', data['date_from']),
             ('Date', '<=',data['date_to'])])
 
-
-        if data['date_from'] and data['date_to'] and not data['customer_id']:
+# Not customers and not state
+        if data['date_from'] and data['date_to'] and not data['customer_id'] and not data['state']:
+            print("!!!!!!!!! Not customers and not stat::::::::::")
             customers = self.env['res.partner'].search([])
-            customer_list =[]
-            print("_____nnn_______________________________",customers)
-            for custom in customers:
-                data_list = []
-                lines_list = []
+            lisy = []
+            for rec in customers:
+                custom_list = [] 
+                customer = self.env['res.partner'].browse(rec)
+                name = rec.name
 
-                for check in check_followups.filtered(lambda r:r.account_holder == custom):
-                    print("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCc",check)
+                lines_list = []
+                for check  in check_followups.filtered(lambda r: r.account_holder == rec):
                     operation_line = []
-                    operation_line = [check.state,check.Date,check.check_no,check.payment_id.check_date,check.payment_id.journal_id.name,check.amount]
+                    # get state
+                    state = ''
+                    if check.state == 'under_collection':
+                        state = 'Under Collection'
+                    if check.state == 'in_bank':
+                        state = 'In Bank'
+                    if check.state in ['rdc','rdv']:
+                        state = 'Check Rejected'
+                    if check.state in ['return_acc','return_acv']:
+                        state = 'Return to Partner'
+                    if check.state in ['donec','donev']:
+                        state = 'Done'
+                    if check.state == 'out_standing':
+                        state = 'Out Standing'
+                    if check.state == 'withdrawal':
+                        state = 'Withdraw From Bank'
+                    if check.state == 'cancel':
+                        state = 'Cancel'
+                    operation_line = [state,check.Date,check.check_no,check.payment_id.check_date,check.payment_id.journal_id.name,check.amount]
                     lines_list.append(operation_line)
+
+           
+                lines_dict = {}
+                for rec in lines_list:
+
+                    if rec[0] in lines_dict:
+                        lines_dict[rec[0]].append([rec[1],rec[2],rec[3],rec[4],rec[5]])
+
+                    else:
+
+                        lines_dict[rec[0]] = [[rec[1],rec[2],rec[3],rec[4],rec[5]]]
+                if lines_dict:       
+                    lisy.append({
+
+                        'name': name,
+                        'list': lines_dict,
+                        })
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",lisy)
+
+            return lisy
+
+# customers and not state
+        if data['date_from'] and data['date_to'] and data['customer_id'] and not data['state']:
+            print("$################ customers and not state")
+            lisy = []
+            customers = data['customer_id']
+            for rec in customers:
+                custom_list = [] 
+                customer = self.env['res.partner'].browse(rec)
+                name = customer.name
+
+                lines_list = []
                 
+                for check  in check_followups.filtered(lambda r: r.account_holder.id == rec):
+                    # get state
+                    state = ''
+                    if check.state == 'under_collection':
+                        state = 'Under Collection'
+                    if check.state == 'in_bank':
+                        state = 'In Bank'
+                    if check.state in ['rdc','rdv']:
+                        state = 'Check Rejected'
+                    if check.state in ['return_acc','return_acv']:
+                        state = 'Return to Partner'
+                    if check.state in ['donec','donev']:
+                        state = 'Done'
+                    if check.state == 'out_standing':
+                        state = 'Out Standing'
+                    if check.state == 'withdrawal':
+                        state = 'Withdraw From Bank'
+                    if check.state == 'cancel':
+                        state = 'Cancel'
+
+                    operation_line = []
+                    operation_line = [state,check.Date,check.check_no,check.payment_id.check_date,check.payment_id.journal_id.name,check.amount]
+                    lines_list.append(operation_line)
+
+                print("__________________________________line",lines_list)
+                # print("__________________________________lineDi",lines_dict)
                 lines_dict = {}
                 for rec in lines_list:
 
@@ -80,80 +136,174 @@ class ChequeReport(models.AbstractModel):
                         lines_dict[rec[0]] = [[rec[1],rec[2],rec[3],rec[4],rec[5]]]
 
 
+                print("___222_______________________________lineDi",lines_dict)
+
+
+                lisy.append({
+
+                    'name': name,
+                    'list': lines_dict,
+                    })
+
+            return lisy
+
+# Not customers and state
+        if data['date_from'] and data['date_to'] and not data['customer_id'] and data['state']:
+            if data['state'] == 'reject':
+                check_followups = self.env['check_followups.check_followups'].search([
+                    ('Date', '>=', data['date_from']),
+                    ('Date', '<=',data['date_to']),
+                    ('state','in',['rdc','rdv'])])
+
+            elif data['state'] == 'return':
+                check_followups = self.env['check_followups.check_followups'].search([
+                    ('Date', '>=', data['date_from']),
+                    ('Date', '<=',data['date_to']),
+                    ('state','in',['return_acc','return_acc'])])
+
+            elif data['state'] == 'done':
+                check_followups = self.env['check_followups.check_followups'].search([
+                    ('Date', '>=', data['date_from']),
+                    ('Date', '<=',data['date_to']),
+                    ('state','in',['donec','donev'])])
+
+            customers = self.env['res.partner'].search([])
+            lisy = []
+            for rec in customers:
+                custom_list = []
+                name = rec.name
+                lines_list = []
+                print("___________________________",rec)
+
+                print("@################################333",check_followups)
+                if data['state'] == 'return' or data['state'] == 'reject' or data['state'] == 'done':
+                    followups = check_followups.filtered(lambda r: r.account_holder == rec)
+                else:
+                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!else")
+                    followups = check_followups.filtered(lambda r: r.account_holder == rec and r.state == data['state'])
+                    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$",data['state'],followups)
                 
-                # if lines_dict:
-                #     customer_list.append({
-                #         'custom':custom.name,
-                #         'data_list':lines_dict,                    
-                #         })
 
-            # if lines_dict:
-                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11lines_dict",lines_dict)
-            return lines_dict
+                print("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVv",check_followups)
+                for check  in followups:
+                    # get state
+                    state = ''
+                    if check.state == 'under_collection':
+                        state = 'Under Collection'
+                    if check.state == 'in_bank':
+                        state = 'In Bank'
+                    if check.state in ['rdc','rdv']:
+                        state = 'Check Rejected'
+                    if check.state in ['return_acc','return_acv']:
+                        state = 'Return to Partner'
+                    if check.state in ['donec','donev']:
+                        state = 'Done'
+                    if check.state == 'out_standing':
+                        state = 'Out Standing'
+                    if check.state == 'withdrawal':
+                        state = 'Withdraw From Bank'
+                    if check.state == 'cancel':
+                        state = 'Cancel'
 
-        if data['date_from'] and data['date_to'] and data['customer_id']:
-            print("MMMMMMMMMMMMMMMMMMMMMMMMMMMmm")
-
-  
-            lines_list = []
-            for check  in check_followups.filtered(lambda r: r.account_holder.id == data['customer_id']):
-                operation_line = []
-                operation_line = [check.state,check.Date,check.check_no,check.payment_id.check_date,check.payment_id.journal_id.name,check.amount]
-                lines_list.append(operation_line)
+                    operation_line = []
+                    operation_line = [state,check.Date,check.check_no,check.payment_id.check_date,check.payment_id.journal_id.name,check.amount]
+                    lines_list.append(operation_line)
 
            
-            lines_dict = {}
-            for rec in lines_list:
+                lines_dict = {}
+                for rec in lines_list:
 
-                if rec[0] in lines_dict:
-                    lines_dict[rec[0]].append([rec[1],rec[2],rec[3],rec[4],rec[5]])
+                    if rec[0] in lines_dict:
+                        lines_dict[rec[0]].append([rec[1],rec[2],rec[3],rec[4],rec[5]])
 
-                else:
+                    else:
 
-                    lines_dict[rec[0]] = [[rec[1],rec[2],rec[3],rec[4],rec[5]]]
+                        lines_dict[rec[0]] = [[rec[1],rec[2],rec[3],rec[4],rec[5]]]
+                if lines_dict:       
+                    lisy.append({
 
+                        'name': name,
+                        'list': lines_dict,
+                        })
 
-            return lines_dict
+            return lisy
 
-
+# Customer and State
         if data['date_from'] and data['date_to'] and data['customer_id'] and data['state']:
-            print("MMMMMMMMMMMMMMMMMMMMMMMMMMMmm")
+            if data['state'] == 'reject':
+                check_followups = self.env['check_followups.check_followups'].search([
+                    ('Date', '>=', data['date_from']),
+                    ('Date', '<=',data['date_to']),
+                    ('state','in',['rdc','rdv'])])
 
+            elif data['state'] == 'return':
+                check_followups = self.env['check_followups.check_followups'].search([
+                    ('Date', '>=', data['date_from']),
+                    ('Date', '<=',data['date_to']),
+                    ('state','in',['return_acc','return_acc'])])
+
+            elif data['state'] == 'done':
+                check_followups = self.env['check_followups.check_followups'].search([
+                    ('Date', '>=', data['date_from']),
+                    ('Date', '<=',data['date_to']),
+                    ('state','in',['donec','donev'])])
+
+
+            lisy = []
             customers = data['customer_id']
             for rec in customers:
                 custom_list = [] 
                 customer = self.env['res.partner'].browse(rec)
                 name = customer.name
 
-            lines_list = []
-            for check  in check_followups.filtered(lambda r: r.account_holder.id == data['customer_id'] and r.state == data['state']):
-                custom_list.append({
-                       'name': check.Date,
-                       # 'date_order': order.date_order,
-                       # 'date_planned': order.date_planned,
-                       # 'product_id': line.product_id.name,
-                       # 'product_qty': line.product_qty,
-                       # 'product_uom': line.product_uom.name,
-                       # 'price_unit': line.price_unit,
-                  
-                    })
-                operation_line = []
-                operation_line = [check.state,check.Date,check.check_no,check.payment_id.check_date,check.payment_id.journal_id.name,check.amount]
-                lines_list.append(operation_line)
+                lines_list = []
+                if data['state'] == 'return' or data['state'] == 'reject' or data['state'] == 'done':
+                    followups = check_followups.filtered(lambda r: r.account_holder.id == rec)
+                else:
+                    followups = check_followups.filtered(lambda r: r.account_holder.id == rec and r.state == data['state'])
+                for check  in followups:
+                    # get state
+                    state = ''
+                    if check.state == 'under_collection':
+                        state = 'Under Collection'
+                    if check.state == 'in_bank':
+                        state = 'In Bank'
+                    if check.state in ['rdc','rdv']:
+                        state = 'Check Rejected'
+                    if check.state in ['return_acc','return_acv']:
+                        state = 'Return to Partner'
+                    if check.state in ['donec','donev']:
+                        state = 'Done'
+                    if check.state == 'out_standing':
+                        state = 'Out Standing'
+                    if check.state == 'withdrawal':
+                        state = 'Withdraw From Bank'
+                    if check.state == 'cancel':
+                        state = 'Cancel'
+
+                    operation_line = []
+                    operation_line = [state,check.Date,check.check_no,check.payment_id.check_date,check.payment_id.journal_id.name,check.amount]
+                    lines_list.append(operation_line)
 
            
-            # lines_dict = {}
-            # for rec in lines_list:
+                lines_dict = {}
+                for rec in lines_list:
 
-            #     if rec[0] in lines_dict:
-            #         lines_dict[rec[0]].append([rec[1],rec[2],rec[3],rec[4],rec[5]])
+                    if rec[0] in lines_dict:
+                        lines_dict[rec[0]].append([rec[1],rec[2],rec[3],rec[4],rec[5]])
 
-            #     else:
+                    else:
 
-            #         lines_dict[rec[0]] = [[rec[1],rec[2],rec[3],rec[4],rec[5]]]
+                        lines_dict[rec[0]] = [[rec[1],rec[2],rec[3],rec[4],rec[5]]]
 
+                if lines_dict:     
+                    lisy.append({
 
-            return lines_dict
+                        'name': name,
+                        'list': lines_dict,
+                        })
+
+            return lisy
     @api.model
     def _get_report_values(self, docids, data=None):
         data['records'] = self.env['check_followups.check_followups'].browse(data)
--- zaway_account_custom/wizard/cheque_wizard.py
+++ zaway_account_custom/wizard/cheque_wizard.py
@@ -11,28 +11,16 @@ class ChequeWizard(models.TransientModel):
 
     date_from = fields.Date(string="From",required=True)
     date_to = fields.Date(string="To",required=True)
-    # customer_id = fields.Many2many('res.partner',string="Customer")
     customer_id = fields.Many2many('res.partner','rel_partner','customer_id','user_id',string="Customer")
-    # under_collection = fields.Boolean(string="Under Collection")
-    # in_bank = fields.Boolean(string="In Bank")
-    # rejected = fields.Boolean(string="Check Rejected")
-    # return_to = fields.Boolean(string="Return to Partner")
-    # out_standing = fields.Boolean(string="Out Standing")
-    # withdrawal = fields.Boolean(string="Withdraw From Bank")
-    # done = fields.Boolean(string="Done")
-    # cancel = fields.Boolean(string="Canceled")
 
     state = fields.Selection([
         ('under_collection', 'Under Collection'),
         ('in_bank', 'In Bank'),
-        ('rdc', 'Check Rejected'),
-        ('return_acc', 'Return to Partner'),
-        ('donec', 'Done'),
+        ('reject', 'Check Rejected'),
+        ('return', 'Return to Partner'),
+        ('done', 'Done'),
         ('out_standing', 'Out Standing'),
         ('withdrawal', 'Withdraw From Bank'),
-        ('rdv', 'Check Rejected'),
-        ('return_acv', 'Return to Partner'),
-        ('donev', 'Done'),
         ('cancel', 'Canceled')],string="State")
 
     # company_id = fields.Many2one('res.company', string="Company", default=lambda self: self.env.user.company_id)
@@ -45,32 +33,8 @@ class ChequeWizard(models.TransientModel):
             'model': self._name,
             'date_from': fields.Date.from_string(self.date_from),
             'date_to': fields.Date.from_string(self.date_to),
-            'customer_id': self.customer_id.id,
-            'customer_name': self.customer_id.name,
-            # 'under_collection':  self.under_collection,
-            # 'in_bank' : self.in_bank,
-            # 'rejected' : self.rejected,
-            # 'out_standing':  self.out_standing,
-            # 'return_to' : self.return_to,
-            # 'withdrawal': self.withdrawal,
-            # 'done': self.done,
-            # 'cancel' :self.cancel,
+            'customer_id': self.customer_id.ids,
             'state': self.state,
-            # 'company_id' : self.company_id.id, 
             }
-        # data = {}
-
-        print("******************************8888",self.date_from,self.customer_id)
-        # data['date_from'] = self.date_from
-        # data['date_to'] = self.date_to
-        # date['customer'] = self.customer_id.id
-        # date['under_collection'] = self.under_collection
-        # date['in_bank'] = self.in_bank
-        # date['rejected'] = self.rejected
-        # date['out_standing'] = self.out_standing
-        # date['withdrawal'] = self.withdrawal
-        # date['done'] = self.done
-        # date['cancel'] = self.cancel
-        # data['comp'] = self.company_id.id
 
         return self.env.ref('zaway_account_custom.cheque_report_id').report_action([], data=data)
